cb13c2efdbc8dcfa80bf6eb34448bbb06d43fb0f
Shay <shay.vandam96@gmail.com>
2021-07-09 10:01:54 +0100

    Add background job for posting tweets

---
+++ app/jobs/tweet_job.rb
@@ -0,0 +1,26 @@
+class TweetJob < ApplicationJob
+  queue_as :default
+
+  def perform(tweet)
+    return if tweet.published?
+
+    # Rescheduled a tweet to the future
+    return if tweet.published_at > Time.current
+
+    tweet.publish_to_twitter!
+  end
+
+end
+
+# Push the publish_at time back
+# noon -> 8am
+# -
+# 8am -> sets tweet id
+# Noon -> already published, does nothing
+
+
+# Push the publish_at time forward
+# 9am -> 1pm
+# -
+# 9am -> should do nothing
+# 1pm should publish the tweet and set the tweet id
