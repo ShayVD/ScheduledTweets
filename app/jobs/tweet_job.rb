0b86be0d302f0f6f26d664d0e7afb139f42b402b
Shay <shay.vandam96@gmail.com>
2021-07-09 11:11:06 +0100

    Add Sidekiq for Background Jobs

--- app/jobs/tweet_job.rb
+++ app/jobs/tweet_job.rb
@@ -5,7 +5,7 @@ class TweetJob < ApplicationJob
     return if tweet.published?
 
     # Rescheduled a tweet to the future
-    return if tweet.published_at > Time.current
+    return if tweet.publish_at > Time.current
 
     tweet.publish_to_twitter!
   end
